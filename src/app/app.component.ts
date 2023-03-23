12f72b2f84dcb8de92c65fcb94df7c0285c79572
filipecajado <filipecajado@ucl.br>
2023-03-22 23:33:19 -0300

    Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,13 +44,15 @@ AfterViewChecked
   <br>
   <button (click)="add()">Add</button> --> 
 
-  <ng-template [ngIf]="getDados">
+  <!-- <ng-template [ngIf]="getDados"> 
     <h1>{{getDados.nome}}</h1>
     <h2>{{getDados.idade}}</h2>
   </ng-template>
-  <app-output (enviarDados)="setDados($event)"></app-output>
-
-  <router-outlet></router-outlet>`
+  <app-output (enviarDados)="setDados($event)"></app-output>-->
+  <app-food-add></app-food-add>
+  <app-food-list></app-food-list>
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent implements OnInit {
 
